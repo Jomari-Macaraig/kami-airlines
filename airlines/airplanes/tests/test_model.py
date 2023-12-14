a74285712508053410f63ecaf41cbee907bb5258
Jomari Macaraig <jomari.macaraig@firstperformance.com>
2023-12-14 22:53:46 +0800

    Added tests

---
+++ airlines/airplanes/tests/test_model.py
@@ -0,0 +1,69 @@
+import math
+from decimal import Decimal
+
+from django.test import TestCase
+
+from ..models import Airplane
+
+
+class AirplaneModelTests(TestCase):
+
+    def setUp(self) -> None:
+        self.airplane, _ = Airplane.objects.get_or_create(
+            id=2,
+            name="Test Airplane",
+            passenger=80,
+        )
+
+    def get_airplane_fuel_consumption_per_minute(self) -> Decimal:
+        return Decimal(math.log(self.airplane.id, 10)) * self.airplane.FUEL_CONSUMPTION
+
+    def get_additional_fuel_consumption_from_passenger_per_minute(self) -> Decimal:
+        return self.airplane.passenger * self.airplane.PASSENGER_FUEL_CONSUMPTION
+
+    def test_fuel_tank_capacity(self) -> None:
+        expected_result = self.airplane.MINIMUM_FUEL * self.airplane.id
+        actual_result = self.airplane.get_fuel_tank_capacity()
+
+        self.assertEqual(
+            actual_result,
+            expected_result,
+            f"Expected result is {expected_result} but method returns {actual_result}"
+        )
+
+    def test_airplane_fuel_consumption_per_minute(self) -> None:
+        expected_result = self.get_airplane_fuel_consumption_per_minute()
+        actual_result = self.airplane.get_airplane_fuel_consumption_per_minute()
+
+        self.assertEqual(
+            actual_result,
+            expected_result,
+            f"Expected result is {expected_result} but method returns {actual_result}"
+        )
+
+    def test_additional_fuel_consumption_from_passenger_per_minute(self) -> None:
+        expected_result = self.get_additional_fuel_consumption_from_passenger_per_minute()
+        actual_result = self.airplane.get_additional_fuel_consumption_from_passenger_per_minute()
+
+        self.assertEqual(
+            actual_result,
+            expected_result,
+            f"Expected result is {expected_result} but method returns {actual_result}"
+        )
+
+    def test_total_fuel_consumption_per_minute(self) -> None:
+        airplane_fuel_consumption_per_minute = self.get_airplane_fuel_consumption_per_minute()
+        additional_fuel_consumption_from_passenger_per_minute = (
+            self.get_additional_fuel_consumption_from_passenger_per_minute()
+        )
+        expected_result = airplane_fuel_consumption_per_minute + additional_fuel_consumption_from_passenger_per_minute
+        actual_result = self.airplane.get_total_fuel_consumption_per_minute()
+
+        self.assertEqual(
+            actual_result,
+            expected_result,
+            f"Expected result is {expected_result} but method returns {actual_result}"
+        )
+
+    def tearDown(self) -> None:
+        self.airplane.delete()
